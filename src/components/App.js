514810fd0ad120d7f6a0d9a72ad66a40582bde2e
Cathy Richardson <cathyanne08@gmail.com>
2021-07-01 20:49:03 -0600

    got to number 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import axios from 'axios'
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post';
 
 class App extends Component {
   constructor() {
@@ -13,17 +15,30 @@ class App extends Component {
       posts: []
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    console.log("in componentDidMount");
+    axios.get('https://practiceapi.devmountain.com/api/posts')
+      .then(res => {
+        this.setState({ posts: res.data })
+      })
+      .catch(err => {
+        err.message
+      })
   }
 
-  updatePost() {
-  
+  updatePost(id, body) {
+    axios.put(`https://practiceapi.devmountain.com/api/posts?${id}, ${body}`)
+    .then( ({data}) => {
+      this.setState({posts: data})
+    })
+    .catch( err => {
+      err.message
+    })
   }
 
   deletePost() {
@@ -36,6 +51,9 @@ class App extends Component {
 
   render() {
     const { posts } = this.state;
+    console.log("posts array: ", posts);
+   
+    let renderPosts = posts.map(el => <Post key={el.id} text={el.text} date={el.date} updatePostFn={this.updatePost} id={el.id}/>)
 
     return (
       <div className="App__parent">
@@ -44,7 +62,7 @@ class App extends Component {
         <section className="App__content">
 
           <Compose />
-          
+          {renderPosts}
         </section>
       </div>
     );
